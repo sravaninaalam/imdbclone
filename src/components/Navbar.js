8c67eed438242fbdfbc496e604696e14214ee868
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-07-01 22:10:10 +0530

    navbar✅

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,30 @@
+'use client'
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function Navbar() {
   return (
     <>
-        <ul className="bg-lime-200 flex p-2 justify-center gap-7">
-          <Link href='/trending'><li>Trending</li></Link>
-          <Link href='/toprated'><li>TopRated</li></Link>
-        </ul>
+        <div className="bg-lime-200 flex p-2 justify-center gap-7">
+          <NavbarItems title='Trending' param='trending'/>
+          <NavbarItems title='TopRated' param='toprated'/>
+        
+        </div>
     </>
   )
+}
+export  function NavbarItems({title,param}) {
+  const searchparams=useSearchParams()
+  const genre = searchparams.get('genre');
+  return (
+    <div>
+       <Link href={`/?genre=${param}`} className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+            : ''
+        }`}>
+        <p>{title}</p>
+       </Link>
+    </div>
+  );
 }
