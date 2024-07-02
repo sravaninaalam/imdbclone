40006680e95d96e85f9ad0568bca119fafe00496
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-07-02 21:08:37 +0530

    root page loading ✅

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,7 +1,10 @@
 export default function loading() {
-    return (
-        <div>
-            Enter
+ 
+    const arr=Array(15).fill("")
+    return(
+        <div className="flex flex-wrap max-w-6xl mx-auto">
+            {arr.map((ar,index)=><div key={index} className="w-72 h-52 bg-gray-300 rounded-md m-4"></div>)}
         </div>
+
     );
 }
