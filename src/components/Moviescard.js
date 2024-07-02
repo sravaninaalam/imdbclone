036a892ea415ed781bbecef13931b5445f95bd18
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-07-02 21:30:32 +0530

    commit

--- src/components/Moviescard.js
+++ src/components/Moviescard.js
@@ -5,7 +5,7 @@ export default function Moviescard({movie}) {
         <>
         <div className="w-64  mx-2  my-4 rounded-md border border-black ">
         <Link href={`/movie/${movie.imdbid}`}>
-            <Image src={movie.image} width={200} height={50} className="rounded-md mx-auto mt-4"/>
+            <Image src={movie.image} width={200} height={50} alt='movie poster' className="rounded-md mx-auto mt-4"/>
             <p className="truncate m-1 text-sm">{movie.description}</p>
             <h1 className="p-1 m-1 font-semibold truncate ">{movie.title}</h1>
             <p className="p-1 mx-1 text-sm">{movie.genre.join(',')}</p> 
